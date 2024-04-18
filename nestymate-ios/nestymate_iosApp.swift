d56cdea3229de59b81c85b33f6319d4f3f744635
Selini Kyriazidou <kselini@gmail.com>
2024-04-18 11:16:26 +0300

    create xcode project

---
+++ nestymate-ios/nestymate_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  nestymate_iosApp.swift
+//  nestymate-ios
+//
+//  Created by Selini Kyriazidou on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct nestymate_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
