b00ea802513885a2a37be447943adc27f084f263
Selini Kyriazidou <kselini@gmail.com>
2024-09-15 13:12:02 +0300

    Show mainScreen if token exist

---
+++ nestymate-ios/Presentation/Root/MainViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewModel.swift
+//  nestymate-ios
+//
+//  Created by Selini Kyriazidou on 15/9/24.
+//
+
+import Foundation
+
+class MainViewModel: ObservableObject {
+    let keyChain = KeychainHelper()
+    var shouldShowLogin: LoginPage {
+        guard keyChain.read() != nil else { return .login }
+        return .mainScreen
+    }
+}
