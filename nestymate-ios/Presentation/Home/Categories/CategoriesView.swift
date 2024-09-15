15deca373e8ad67badf1b79795473d20985723f7
Selini Kyriazidou <kselini@gmail.com>
2024-09-15 14:17:13 +0300

    Create Create Categories Screen

--- nestymate-ios/Presentation/Home/Categories/CategoriesView.swift
+++ nestymate-ios/Presentation/Home/Categories/CategoriesView.swift
@@ -8,15 +8,47 @@
 import SwiftUI
 
 struct CategoriesView: View {
+    @ObservedObject var viewModel: CategoryViewModel = .init()
+    @State private var viewDidLoad = false
+    @State private var categories: [Category] = []
+    struct Output {
+        var createCategory: () -> Void
+    }
+
+    var output: Output
     var body: some View {
         VStack {
-            Text("Categories")
-            Spacer()
+            HStack(alignment: .center) {
+                Text("Categories").font(FontManager.title)
+                Spacer()
+                Button {
+                    self.output.createCategory()
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .padding()
+
+            List {
+                ForEach(categories) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: viewModel.delete)
+            }
+            .padding()
         }
         .background(ColorManager.backgroundColour)
+        .onAppear {
+            if viewDidLoad == false {
+                viewDidLoad = true
+                viewModel.getCategories { categories in
+                    self.categories = categories
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    CategoriesView()
+    CategoriesView(output: CategoriesView.Output(createCategory: {}))
 }
---
+++ nestymate-ios/Presentation/Home/Categories/CreateCategory/CreateCategoryViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  CreateCategoryViewModel.swift
+//  nestymate-ios
+//
+//  Created by Selini Kyriazidou on 15/9/24.
+//
+
+import Foundation
+
+class CreateCategoryViewModel: ObservableObject {
+    let useCase: CategoryUseCase = CategoryUseCaseImpl()
+    @Published public var name: FieldModel = .init(value: "", fieldType: .name)
+    @Published public var shouldShowLoader: Bool?
+    @Published var error: Error?
+
+    public func createCategory(completionHandler: @escaping () -> Void) {
+        shouldShowLoader = true
+        useCase.createCategory { [weak self] error in
+            guard let self else { return }
+            self.shouldShowLoader = false
+            self.error = error
+            completionHandler()
+        }
+    }
+}
