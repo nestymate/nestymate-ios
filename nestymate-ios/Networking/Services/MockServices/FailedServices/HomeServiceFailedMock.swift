f8382df90657f16f3544c97c67ff27bb7593b7ab
Selini Kyriazidou <kselini@gmail.com>
2024-11-22 00:05:19 +0200

    Add tests for login

---
+++ nestymate-ios/Networking/Services/MockServices/FailedServices/HomeServiceFailedMock.swift
@@ -0,0 +1,26 @@
+//
+//  HomeServiceFailedMock.swift
+//  nestymate-ios
+//
+//  Created by Selini Kyriazidou on 22/11/24.
+//
+
+import Foundation
+
+final class HomeServiceFailedMock: HomeService {
+    func inviteUserToHome(email _: String, completionHandler: @escaping (Error?, Int?) -> Void) {
+        completionHandler(nil, 500)
+    }
+
+    func createHome(home _: Home, completionHandler: @escaping (Error?, Int?) -> Void) {
+        completionHandler(nil, 500)
+    }
+
+    func editHome(home _: Home, completionHandler: @escaping (Error?, Int?) -> Void) {
+        completionHandler(nil, 500)
+    }
+
+    func getHome(completionHandler: @escaping (Home?, Error?, Int?) -> Void) {
+        completionHandler(nil, nil, 500)
+    }
+}
