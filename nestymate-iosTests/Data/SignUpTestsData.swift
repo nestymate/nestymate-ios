0824ffcd0911308c30c8fbf0b0b55c9a0af015b5
Selini Kyriazidou <kselini@gmail.com>
2024-12-28 01:36:30 +0200

    Create tests for sign up

---
+++ nestymate-iosTests/Data/SignUpTestsData.swift
@@ -0,0 +1,55 @@
+//
+//  SignUpTestsData.swift
+//  nestymate-ios
+//
+//  Created by Selini Kyriazidou on 27/12/24.
+//
+
+@testable import nestymate_ios
+
+class SignUpTestsData {
+    static let emptyValuesUser = User(
+        username: "",
+        password: "",
+        repeatPassword: "",
+        name: "",
+        surname: "",
+        birthday: "",
+        gender: ""
+    )
+    static let correctUser = User(
+        username: "Selini",
+        password: "Sadface123!",
+        repeatPassword: "Sadface123!",
+        name: "Seli", surname: "Kyriaz",
+        birthday: "1992-12-27",
+        gender: "other"
+    )
+
+    static let incorrectPasswordUser = User(
+        username: "Selini",
+        password: "123",
+        repeatPassword: "123",
+        name: "Seli", surname: "Kyriaz",
+        birthday: "1992-12-27",
+        gender: "other"
+    )
+
+    static let notMatchingPasswordUser = User(
+        username: "Selini",
+        password: "Sadface123!",
+        repeatPassword: "Sadface123!!",
+        name: "Seli", surname: "Kyriaz",
+        birthday: "1992-12-27",
+        gender: "other"
+    )
+
+    static let incorrectBirthdayUser = User(
+        username: "Selini",
+        password: "Sadface123!",
+        repeatPassword: "Sadface123!",
+        name: "Seli", surname: "Kyriaz",
+        birthday: DateUtils.getToday(),
+        gender: "other"
+    )
+}
