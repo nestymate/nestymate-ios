6dfdefdf2f844f97522589e6cb12296d2d9496a3
Selini Kyriazidou <kselini@gmail.com>
2025-02-03 20:20:02 +0200

    Create test for home

--- nestymate-ios/Presentation/Home/MyHome/HomeUseCase.swift
+++ nestymate-ios/Presentation/Home/MyHome/HomeUseCase.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 protocol HomeUseCase {
-    func inviteUserToHome(email: String, completionHandler: @escaping (Error?, Int?) -> Void)
+    func createValid(isNameValid: Bool, isDescriptionValid: Bool, isAddressValid: Bool) -> Bool
     func getHome(completionHandler: @escaping (Home?, Error?, Int?) -> Void)
     func editHome(home: Home, completionHandler: @escaping (Error?, Int?) -> Void)
-    func createValid(isNameValid: Bool, isDescriptionValid: Bool, isAddressValid: Bool) -> Bool
     func createHome(home: Home, completionHandler: @escaping (Error?, Int?) -> Void)
+    func inviteUserToHome(email: String, completionHandler: @escaping (Error?, Int?) -> Void)
 }
 
 class HomeUseCaseImpl: HomeUseCase {
@@ -34,11 +34,11 @@ class HomeUseCaseImpl: HomeUseCase {
         homeService.editHome(home: home, completionHandler: completionHandler)
     }
 
-    func inviteUserToHome(email: String, completionHandler: @escaping (Error?, Int?) -> Void) {
-        homeService.inviteUserToHome(email: email, completionHandler: completionHandler)
-    }
-
     func createHome(home: Home, completionHandler: @escaping (Error?, Int?) -> Void) {
         homeService.createHome(home: home, completionHandler: completionHandler)
+    }
+
+    func inviteUserToHome(email: String, completionHandler: @escaping (Error?, Int?) -> Void) {
+        homeService.inviteUserToHome(email: email, completionHandler: completionHandler)
     }
 }
