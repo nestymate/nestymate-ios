a70fa781561028bb161f9ee686d8c7ba11e7353b
Selini Kyriazidou <kselini@gmail.com>
2025-02-03 20:20:02 +0200

    Create test for home

--- nestymate-ios/Networking/Services/MockServices/FailedServices/HomeServiceFailedMock.swift
+++ nestymate-ios/Networking/Services/MockServices/FailedServices/HomeServiceFailedMock.swift
@@ -9,15 +9,15 @@ import Foundation
 
 final class HomeServiceFailedMock: HomeService {
     func inviteUserToHome(email _: String, completionHandler: @escaping (Error?, Int?) -> Void) {
-        completionHandler(nil, 500)
+        completionHandler(.badServerResponse, 500)
     }
 
     func createHome(home _: Home, completionHandler: @escaping (Error?, Int?) -> Void) {
-        completionHandler(nil, 500)
+        completionHandler(.badServerResponse, 500)
     }
 
     func editHome(home _: Home, completionHandler: @escaping (Error?, Int?) -> Void) {
-        completionHandler(nil, 500)
+        completionHandler(.badServerResponse, 500)
     }
 
     func getHome(completionHandler: @escaping (Home?, Error?, Int?) -> Void) {
---
+++ nestymate-iosTests/Data/HomeTestsData.swift
@@ -0,0 +1,12 @@
+//
+//  HomeTestsData.swift
+//  nestymate-ios
+//
+//  Created by Selini Kyriazidou on 3/2/25.
+//
+
+@testable import nestymate_ios
+
+class HomeTestsData {
+    static let home = Home(id: 0, name: "test", description: "home", address: "unkon 1")
+}
